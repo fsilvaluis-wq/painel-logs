08180f843e24eb21a75f658f7975e8346fa84672
Luis Fernando <fsilvaluis@icloud.com>
2026-03-10 19:22:38 -0300

    corrige rota frontend

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../web/dist")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../web/dist/index.html"));
 });
 
