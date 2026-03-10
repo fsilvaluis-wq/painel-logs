964df8636cde07856a4a949421e9fa5f927e987f
Luis Fernando <fsilvaluis@icloud.com>
2026-03-10 18:36:10 -0300

    rota inicial adicionada

--- src/server.js
+++ src/server.js
@@ -18,6 +18,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Servidor online");
+});
+
 app.get("/health", (req, res) => {
   res.json({ ok: true, message: "API online" });
 });
